14ebf74fefd0528c5847131832bd2c5cd1afd821
Mohd Ejaz Siddiqui <mohdejazsiddiqui@gmail.com>
2020-09-13 00:05:03 +0400

    first cube

--- js/app.js
+++ js/app.js
@@ -5,12 +5,23 @@ init();
 function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xaaaaaa)
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+        color: new THREE.Color('red')
+    });
+    const box = new THREE.Mesh(geometry, material);
+
+    scene.add(box);
+
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
     )
+
+    camera.position.z = 5;
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
