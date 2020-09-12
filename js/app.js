2940f6dba5167ee56aae2a54760ab0e5288398c4
Mohd Ejaz Siddiqui <mohdejazsiddiqui@gmail.com>
2020-09-12 23:48:57 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var scene, camera, renderer;
+
+init();
+
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xaaaaaa)
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onResize, false );
+    update();
+}
+
+function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    renderer.render(scene, camera);
+}
